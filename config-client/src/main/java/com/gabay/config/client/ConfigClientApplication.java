19b12516aad9e039ce7a53612af7c6adecb1f1b0
Lexi214 <307731709@qq.com>
2024-10-22 19:17:36 +0800

    config注册到eureka

--- config-client/src/main/java/com/gabay/config/client/ConfigClientApplication.java
+++ config-client/src/main/java/com/gabay/config/client/ConfigClientApplication.java
@@ -2,9 +2,7 @@ package com.gabay.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient // 开启 Eureka 客户端
 @SpringBootApplication
 public class ConfigClientApplication {
 
